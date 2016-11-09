b6934dd550c60526f7d528e7097d4509d8c83aa7
Hannah McLaughlin <h@mcla.ug>
2016-11-09 14:05:34 +0000

    tested for triangle equality, all pass

--- triangle.py
+++ triangle.py
@@ -1,2 +1,21 @@
+import itertools
+
+
 def is_triangle(a, b, c):
-    return not any(item <= 0 for item in [a, b, c])
+    """Given three line lengths, can these lines form a valid triangle?"""
+    positive = not any(item <= 0 for item in [a, b, c])
+    inequality = triangle_inequality([a, b, c])
+    return positive and inequality
+
+
+def triangle_inequality(lst):
+    i = itertools.cycle([0, 1, 2])
+    j = itertools.cycle([1, 2, 0])
+    k = itertools.cycle([2, 0, 1])
+
+    def inequality_holds():
+        for _ in range(3):
+            a, b, c = lst[next(i)], lst[next(j)], lst[next(k)]
+            yield True if a < (b + c) and a > (b - c) else False
+
+    return all(inequality_holds())
--- triangle_test.py
+++ triangle_test.py
@@ -1,6 +1,8 @@
+import itertools
+import random
+
 from hypothesis import given
 from hypothesis import strategies as st
-import random
 
 import triangle
 
@@ -24,3 +26,21 @@ def test_if_negative_length_return_false(a, b, c):
 def test_if_any_zeros_length_return_false(lst):
     """If one or more of the three lengths are zero, can't be a triangle"""
     assert triangle.is_triangle(*lst) is False
+
+
+@given(st.lists(st.integers(), max_size=3, min_size=3))
+def test_triangle_inequality(lst):
+    """
+    the length of a side of a triangle is less than the sum of the lengths of
+    the other two sides and greater than the difference of the lengths of the
+    other two sides"""
+    i = itertools.cycle([0, 1, 2])
+    j = itertools.cycle([1, 2, 0])
+    k = itertools.cycle([2, 0, 1])
+
+    def inequality_holds():
+        for _ in range(3):
+            a, b, c = lst[next(i)], lst[next(j)], lst[next(k)]
+            yield True if a < (b + c) and a > (b - c) else False
+
+    assert all(inequality_holds()) == triangle.triangle_inequality(lst)
